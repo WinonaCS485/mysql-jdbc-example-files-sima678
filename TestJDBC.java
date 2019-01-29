d916e1457674bd41873cfaccfa33d0552c142432
Mr. Bartucz <jobartucz@isd535.org>
2019-01-29 16:32:47 -0600

    Create TestJDBC.java

---
+++ TestJDBC.java
@@ -0,0 +1,9 @@
+public class testJDBC {
+
+    // This is just a helper program to test your database connection
+    public static void main(String[] args) throws Exception {
+        MySQLAccess dao = new MySQLAccess();
+        dao.readDataBase();
+    }
+
+}
